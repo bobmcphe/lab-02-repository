d193984c0c4bc5b6dbffcb428e387b6a544694b8
bobby@fivemoretalents.com <bobby@fivemoretalents.com>
2020-06-04 14:48:02 -0700

    trying to setup ajax to connect to Json file

--- js/app.js
+++ js/app.js
@@ -1,9 +1,23 @@
-$(document).ready(function(){
-    $.getJSON("data\page-1.json", function(result){
-      $.each(result, function(i, field){
-        $("div").append(field + " ");
-      });
-    });
+// $(document).ready(function(){
+//     $.getJSON("data\page-1.json", function(result){
+//       $.each(result, function(i, field){
+//         $("div").append(field + " ");
+//       });
+//     });
+//   });
+
+
+// let xhr = new XMLHttpRequest();
+// xhr.open('GET', 'data/page-1.json', true);
+
+
+$.ajax({
+    dataType: 'json',
+    url: http://github.com/bobmcphe/lab-02-repository/blob/master/data/page-1.json,
+    data: data,
   });
 
-$.ajax()
+  ajaxRequest.onreadystatechange = function(){
+	console.log("Ready state changed!");
+	//more on this in a second
+}
